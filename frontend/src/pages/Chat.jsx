616ef7dd28cbcab6682a5f91210366f087c4b2a8
AmeerIbdah <s12323137@stu.najah.edu>
2026-05-22 17:51:26 +0300

    some edits

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api";
-import socket from "../socket";
+import api from "../../services/api";
+import socket from "../../services/socket";
 import Sidebar from "../components/Sidebar";
 import MessageBox from "../components/MessageBox";
 
